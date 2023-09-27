29a3afbd8cb848cfea45b9f5b5e6ea3df9ae0aa0
Ieuan Walker <ieuan.walker007@gmail.com>
2023-09-27 17:06:02 +0100

    fix structure

---
+++ Test/Class1.cs
@@ -0,0 +1,35 @@
+﻿using IeuanWalker.Hangfire.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TestSolution;
+
+[RecurringJob("DeleteOldReportsAtMidnight", "* *678 * *")]
+public class DeleteOldReports
+{
+    public void Execute()
+    {
+    }
+}
+
+[RecurringJob("* *678 * *")]
+public class SendConfirmationEmails
+{
+    public void Execute()
+    {
+    }
+}
+
+public class Class2
+{
+    readonly IServiceCollection _serviceCollection;
+    public Class2(IServiceCollection serviceCollection)
+    {
+        _serviceCollection = serviceCollection;
+    }
+    public void Method1()
+    {
+        _serviceCollection.RegisterServicesFromTestSolution();
+    }
+}
+
+
