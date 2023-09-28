281f1b21e93ea3b3b9b4f6757e757d72279b6b92
Ieuan Walker <ieuan.walker007@gmail.com>
2023-09-28 14:37:04 +0100

    update

--- Scr/RecuringJobGenerator - Copy.cs
+++ Scr/RecuringJobGenerator - Copy.cs
@@ -1,33 +1,55 @@
 ﻿using System.Collections.Immutable;
 using System.Text;
-using System.Text.Json;
+using IeuanWalker.Hangfire.Attributes;
+using IeuanWalker.Hangfire.Helpers;
+using IeuanWalker.Hangfire.Interfaces;
+using IeuanWalker.Hangfire.Models;
 using IeuanWalker.Hangfire.Utilties;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 
 namespace IeuanWalker.Hangfire;
 
 [Generator(LanguageNames.CSharp)]
-public class RecuringJobGenerator2 : IIncrementalGenerator
+public class RecuringJobGenerator : IIncrementalGenerator
 {
-	const string attribName = "RecurringJob";
+	static readonly StringBuilder b = new();
+	static string? assemblyName;
+	const string attribShortName = "RecurringJob";
 
-	public virtual void Initialize(IncrementalGeneratorInitializationContext context)
+	/// <summary>
+	/// Starts the generator
+	/// </summary>
+	/// <param name="context"></param>
+	public void Initialize(IncrementalGeneratorInitializationContext context)
 	{
-		var syntaxSymbols = context.SyntaxProvider
-			.CreateSyntaxProvider(
-				predicate: Match,
-				transform: Transform
-			)
-			.Where(item => item is not (null, null))
+		// Constant classes/ interfaces for the users to use
+		context.RegisterPostInitializationOutput(ctx => ctx.AddSource(
+			"RecurringJobAttribute.g.cs",
+			SourceText.From(RecurringJobAttribute.Attribute, Encoding.UTF8)));
+		context.RegisterPostInitializationOutput(ctx => ctx.AddSource(
+			"IRecurringJob.g.cs",
+			SourceText.From(RecurringJobInterface.InterfaceVoid, Encoding.UTF8)));
+		context.RegisterPostInitializationOutput(ctx => ctx.AddSource(
+			"IRecurringJobAsync.g.cs",
+			SourceText.From(RecurringJobInterface.InterfaceAsync, Encoding.UTF8)));
+
+		//Generator implementation
+		var provider = context.SyntaxProvider
+			.CreateSyntaxProvider(Match, Transform)
+			.Where(static r => r is not null)
 			.Collect();
 
-		context.RegisterSourceOutput(syntaxSymbols, Generate);
+		context.RegisterSourceOutput(provider, Generate!);
 	}
 
-
-	protected bool Match(SyntaxNode node, CancellationToken cancellationToken)
+	/// <summary>
+	/// Find all valid classes with the <see cref="RecurringJobAttribute"/> attribute
+	/// </summary>
+	/// <param name="node"></param>
+	/// <param name="_"></param>
+	static bool Match(SyntaxNode node, CancellationToken _)
 	{
 		if (node is not AttributeSyntax attributeSyntax)
 		{
@@ -36,100 +58,123 @@ public class RecuringJobGenerator2 : IIncrementalGenerator
 
 		var name = SyntaxUtil.ExtractName(attributeSyntax.Name);
 
-		return name?.Contains(attribName) ?? false;
+		if (name is null)
+		{
+			return false;
+		}
+
+		return name.Contains(attribShortName);
 	}
 
-	protected virtual (SyntaxNode?, ISymbol?) Transform(GeneratorSyntaxContext context,
-		CancellationToken cancellationToken)
+	/// <summary>
+	/// Transforms all matched classes into <see cref="JobModel"/>s
+	/// </summary>
+	/// <param name="context"></param>
+	/// <param name="_"></param>
+	/// <exception cref="NullReferenceException"></exception>
+	/// <exception cref="Exception"></exception>
+	static JobModel? Transform(GeneratorSyntaxContext context, CancellationToken _)
 	{
 		var attributeSyntax = (AttributeSyntax)context.Node;
 
-		string jsonString = JsonSerializer.Serialize(attributeSyntax);
-
 		// Attribute --> AttributeList --> Class
-		if (attributeSyntax.Parent?.Parent is not ClassDeclarationSyntax classDeclarationSyntax)
+		if (attributeSyntax.Parent?.Parent is not ClassDeclarationSyntax classSyntax)
 		{
-			return (null, null);
+			return default;
 		}
 
 
+		var attributeArguments = attributeSyntax?.ArgumentList?.Arguments;
 
-		var classSymbol = context.SemanticModel
-			.Compilation
-			.GetTypeByMetadataName(
-				SyntaxUtil.GetClassFullname(classDeclarationSyntax)
-			);
+		// We're getting optional parameters
+		var defaultBindingMode = SyntaxUtil.GetAttributeParam(attributeArguments, "TimeZone") ?? "UTC";
 
-		return (classDeclarationSyntax, classSymbol);
+
+
+
+		var service = context.SemanticModel.GetDeclaredSymbol(context.Node);
+
+
+		if (service?.IsAbstract is null or true)
+		{
+			return null;
+		}
+
+		assemblyName = context.SemanticModel.Compilation.AssemblyName;
+
+		string fullClassName = service.ToDisplayString() ?? throw new NullReferenceException();
+
+		var attrib = (context.Node as ClassDeclarationSyntax)!
+			.AttributeLists
+			.SelectMany(al => al.Attributes)
+			.First(a => a.Name.ExtractName()!.Equals(attribShortName));
+
+		if (attrib.ArgumentList!.Arguments.Count != 1 && attrib.ArgumentList!.Arguments.Count != 2)
+		{
+			throw new Exception($"{attribShortName} must have 2 paramenters");
+		}
+
+		var expressions = attrib
+		   .ArgumentList!
+		   .Arguments
+		   .OfType<AttributeArgumentSyntax>()
+		   .Select(x => x.Expression)
+		   .ToList();
+
+		string jobId = string.Empty;
+		string cron = string.Empty;
+		if (expressions.Count == 1)
+		{
+			cron = (string)context.SemanticModel.GetOperation(expressions[0])!.ConstantValue!.Value!;
+		}
+		else
+		{
+			jobId = (string)context.SemanticModel.GetOperation(expressions[0])!.ConstantValue!.Value!;
+			cron = (string)context.SemanticModel.GetOperation(expressions[1])!.ConstantValue!.Value!;
+		}
+
+		if (string.IsNullOrEmpty(jobId))
+		{
+			jobId = fullClassName;
+		}
+
+		return new(fullClassName, jobId, cron, "", "");
 	}
 
-	protected virtual void Generate(SourceProductionContext context,
-		ImmutableArray<(SyntaxNode?, ISymbol?)> syntaxSymbols)
+	/// <summary>
+	/// Generates registration code for all <see cref="JobModel"/>
+	/// </summary>
+	/// <param name="context"></param>
+	/// <param name="jobs"></param>
+	static void Generate(SourceProductionContext context, ImmutableArray<JobModel> jobs)
 	{
-		if (syntaxSymbols.IsDefaultOrEmpty)
+		if (!jobs.Any())
 		{
 			return;
 		}
 
-		var groupList = syntaxSymbols.GroupBy<(SyntaxNode, ISymbol), ClassDeclarationSyntax>(
-			fieldGroup => (ClassDeclarationSyntax)fieldGroup.Item1!.Parent!
-		);
-
-		foreach (var group in groupList)
-		{
-			var sourceCode = ProcessClass(group.Key, group.ToList()).Trim();
-			var className = SyntaxUtil.GetClassFullname(group.Key);
-
-			context.AddSource($"{className}.g.cs", sourceCode);
-		}
-	}
-
-	protected virtual string ProcessClass(ClassDeclarationSyntax? classSyntax,
-		List<(SyntaxNode, ISymbol)> syntaxSymbols)
-	{
-		if (classSyntax is null)
-		{
-			return string.Empty;
-		}
-
-		var usingDirectives = classSyntax.SyntaxTree.GetCompilationUnitRoot().Usings;
-
-		var namespaceSyntax = classSyntax.Parent as BaseNamespaceDeclarationSyntax;
-		var namespaceName = namespaceSyntax?.Name.ToString() ?? "global";
-
-		var source = new StringBuilder($@"
-#pragma warning disable
+		b.Clear().Append(
+"namespace ").Append(assemblyName).Append(@";
 
 // <auto-generated/>
-{usingDirectives}
 
-namespace {namespaceName}
-{{
-    public partial class {classSyntax.Identifier}
-    {{
+using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class RecurringJobRegistrationExtensions
+{
+    public static IServiceCollection RegisterRecurringJobsFrom").Append(assemblyName?.Sanitize(string.Empty) ?? "Assembly").Append(@"(this IServiceCollection sc)
+    {
 ");
-
-		foreach (var (syntax, symbol) in syntaxSymbols)
+		foreach (var job in jobs.OrderBy(r => r!.JobId))
 		{
-			ProcessField(source, classSyntax, syntax, symbol);
+			b.Append("\t\tRecurringJob.AddOrUpdate<").Append(job.FullClassName).Append(">(\"").Append(job.JobId).Append("\"").Append(", x => x.Execute(), \"").Append(job.Cron).Append("\");").Append("\r\n");
 		}
-
-		source.Append(@$"
-    }}
-}}
-");
-
-		return source.ToString();
-	}
-
-	protected virtual string ProcessClass((SyntaxNode, ISymbol) group)
-	{
-		return string.Empty;
-	}
-
-	protected virtual void ProcessField(StringBuilder source, ClassDeclarationSyntax classSyntax,
-		SyntaxNode fieldSyntax, ISymbol fieldSymbol)
-	{
+		b.Append(@"
+        return sc;
+    }
+}");
+		string test = b.ToString();
+		context.AddSource("RecurringJobRegistrationExtensions.g.cs", SourceText.From(b.ToString(), Encoding.UTF8));
 	}
 }
-
