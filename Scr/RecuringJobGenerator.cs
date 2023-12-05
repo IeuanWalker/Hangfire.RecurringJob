115452e57bfe2ea7ed1c3982bcfa44684403199a
Ieuan Walker <ieuan.walker007@gmail.com>
2023-12-05 23:59:05 +0000

    update

--- Scr/RecuringJobGenerator.cs
+++ Scr/RecuringJobGenerator.cs
@@ -60,15 +60,10 @@ public class RecuringJobGenerator : IIncrementalGenerator
 		}
 
 		assemblyName = context.SemanticModel.Compilation.AssemblyName;
-		string? jobId = (string?)attribute.NamedArguments.FirstOrDefault(a => a.Key == "JobId").Value.Value;
-		string? cron = (string?)attribute.NamedArguments.FirstOrDefault(a => a.Key == "Cron").Value.Value;
-		string? queue = (string?)attribute.NamedArguments.FirstOrDefault(a => a.Key == "Queue").Value.Value;
-		string? timeZone = (string?)attribute.NamedArguments.FirstOrDefault(a => a.Key == "TimeZone").Value.Value;
-
-		if (jobId is null || cron is null || queue is null || timeZone is null)
-		{
-			return null;
-		}
+		string jobId = (string?)attribute.NamedArguments.FirstOrDefault(a => a.Key == "JobId").Value.Value ?? "Test"; // TODO: Use class name if jobId is null
+		string cron = (string)attribute.NamedArguments.FirstOrDefault(a => a.Key == "Cron").Value.Value!;
+		string queue = (string)attribute.NamedArguments.FirstOrDefault(a => a.Key == "Queue").Value.Value!;
+		string timeZone = (string)attribute.NamedArguments.FirstOrDefault(a => a.Key == "TimeZone").Value.Value!;
 
 		return new JobModel(context.TargetSymbol.Name, jobId, cron, queue, timeZone);
 	}
